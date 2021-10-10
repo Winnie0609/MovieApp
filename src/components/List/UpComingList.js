fe3cfab0e9001263e747f35511d7af316121c7a2
winnie0609 <winnieong0609@gmail.com>
2021-10-10 17:20:43 +0800

    Updates: Touch up UI

--- src/components/List/UpComingList.js
+++ src/components/List/UpComingList.js
@@ -4,31 +4,31 @@ import { GlobalContext } from '../../context/GlobalState'
 import { ResultCard } from '../allComponent'
 
 function UpComingList() {
-    const { dispatch, state } = useContext(GlobalContext)
-    const key = Math.floor(Math.random() * 10000)
+	const { dispatch, state } = useContext(GlobalContext)
+	const key = Math.floor(Math.random() * 10000)
 
-    useEffect(() => {
-        getUpComingList(dispatch)
-        // getList(1, dispatch, "upComing_movie")
-    }, [])
+	useEffect(() => {
+		getUpComingList(dispatch)
+		// getList(1, dispatch, "upComing_movie")
+	}, [])
 
-    let UpComingList = state.upComing_list
-    
-    return (
-        <div className="up-comming-list"> 
-            {/* <div className="list-title"> */}
-                <h2>Upcoming</h2>
-            {/* </div> */}
-            <div className="result-list">
-                {UpComingList.map((item, index) => (
-                    <div className="up-comming-list-item">
-                        <p className={index <= 2 ? "index index-red" : "index"}>{index + 1}</p>
-                        <ResultCard item={item} key={`${item.id}${key}`} isMovie={true}/>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+	let UpComingList = state.upComing_list
+	
+	return (
+		<div className="up-comming-list"> 
+			{/* <div className="list-title"> */}
+				<h2>Upcoming</h2>
+			{/* </div> */}
+			<div className="result-list">
+				{UpComingList.map((item, index) => (
+					<div className="up-comming-list-item">
+						<p className={index <= 2 ? "index index-red" : "index"}>{index + 1}</p>
+						<ResultCard item={item} key={`${item.id}${key}`} isMovie={true}/>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default UpComingList
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,37 +3,35 @@ import { Header, BannerMovie, PopularList, MovieTrendingList, LatestTrailer, UpC
 import "../style/index.css"
 
 function HomePage() {
-    return(
-        <>
-            <Header />
-            <div className="margin-container">
-                <div className="greeting">
-                    <h1 className="greeting-text">Welcome Back, Karl !</h1>
-                    <p className="greeting-sub">A Movie A Day, Keeps the Doctors Away.</p> 
-                </div>
-                <BannerMovie/>
-                
-                <div className="home-column">
-                    <div className="home-row-one">
-                        <div className="home-popular">
-                            <PopularList />
-                        </div>
-                        <div className="home-trending">
-                            <MovieTrendingList backDrop="true"/>
-                        </div>
-                    </div>
-                    <div className="home-row-two">
-                        <UpComingList />
-                    </div>
-                </div>
-                    <LatestTrailer />
-            </div>
-            
-   
-                <Footer />
+	return(
+		<>
+			<Header />
+			<div className="margin-container">
+				<div className="greeting">
+						<h1 className="greeting-text">Welcome Back, Karl !</h1>
+						<p className="greeting-sub">A Movie A Day, Keeps the Doctors Away.</p> 
+				</div>
+				<BannerMovie/>
+					
+				<div className="home-column column">
+					<div className="home-row-one">
+						<div className="home-popular">
+							<PopularList />
+						</div>
+						<div className="home-trending">
+							<MovieTrendingList backDrop="true"/>
+						</div>
+					</div>
+					<div className="home-row-two">
+						<UpComingList />
+					</div>
+				</div>
+				<LatestTrailer />
+			</div>
 
-        </>
-    )
+			<Footer />
+		</>
+	)
 }
 
 export default HomePage
